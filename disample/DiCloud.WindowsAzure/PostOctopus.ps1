eb5cfd83f89c6be07d9722612383e14a640c93c9
diuser <DITFS\diuser>
2015-04-26 12:18:46 +0000

    fixed the path issue
    git-tfs-id: [http://ditfs.cloudapp.net:8080/tfs/didemocode]$/diprojectsdemo;C34

--- disample/DiCloud.WindowsAzure/PostOctopus.ps1
+++ disample/DiCloud.WindowsAzure/PostOctopus.ps1
@@ -1,5 +1,5 @@
 ﻿Write-Host 'This is coming from post build script'
-Write-Host "Build Number is " $Env: TF_BUILD_BUILDNUMBER
+Write-Host "Build Number is " $Env:TF_BUILD_BUILDNUMBER
 Write-Host "Number of Args:" $args.Length
 foreach ($arg in $args) {Write-Host "Arg : $arg"}
 Write-Host '#################################################'
