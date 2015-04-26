50378c8f2df34046ceb1b209402a275df9a0d5ff
diuser <DITFS\diuser>
2015-04-26 12:44:11 +0000

    Added more
    git-tfs-id: [http://ditfs.cloudapp.net:8080/tfs/didemocode]$/diprojectsdemo;C36

--- disample/DiCloud.WindowsAzure/PostOctopus.ps1
+++ disample/DiCloud.WindowsAzure/PostOctopus.ps1
@@ -1,7 +1,15 @@
 ﻿Write-Host 'This is coming from post build script'
 Write-Host "Build Number is " $Env:TF_BUILD_BUILDNUMBER
-Write-Host "Ms Build Path " $Env:MSBuildBinPath 
-Write-Host "Ms Build Tools Paths $Env:MSBuildToolsPath"
+Write-Host "Source Directory" $Env:TF_BUILD_SOURCESDIRECTORY 
 Write-Host "Number of Args:" $args.Length
 foreach ($arg in $args) {Write-Host "Arg : $arg"}
 Write-Host '#################################################'
+Function Get-MSBuild {
+    $lib = [System.Runtime.InteropServices.RuntimeEnvironment]
+    $rtd = $lib::GetRuntimeDirectory()
+    Join-Path $rtd msbuild.exe
+}
+
+$msbuildPath = Get-MSBuild 
+& cd $Env:TF_BUILD_SOURCESDIRECTORY 
+& $msbuildpath $arg
